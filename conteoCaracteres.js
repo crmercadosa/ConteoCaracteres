c971f2980dcd80e27d23e56e576fff8d2fcf0756
CrissCross20 <165857522+CrissCross20@users.noreply.github.com>
2024-08-28 20:00:44 -0700

    Ejercicio 1. Conteo de caracteres

---
+++ conteoCaracteres.js
@@ -0,0 +1,7 @@
+//Conteo de caracteres: Dada una cadena de texto y luego muestra el número de caracteres que contiene esa cadena.
+
+function contarCaracteres(cadena) {
+    return cadena.length;
+}
+
+console.log(contarCaracteres("Hello world"));
